d376e0804fccecb5a0cb5398c58110f6d725d16d
admin <admin@admin-PC>
2017-10-01 22:06:27 +0530

    prime no program

---
+++ BasicJavaPrograms/src/PrimeNo.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * @author Lilaram Anjane
+ *
+ */
+public class PrimeNo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int k=0;
+		for(int i=1;i<=7;i++)
+		{
+			  if(9%i==0)
+			  {
+				  k++;
+			  }
+		}
+		
+		if(k==2)
+		{
+			System.out.println("no is prime");
+		}
+		
+	}
+
+}
